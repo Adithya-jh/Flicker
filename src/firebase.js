6562af3fd034bb904bc59c46fca3bc8e4c1db9cb
Jayahari Adithya <jhadith88@gmail.com>
2021-09-23 12:05:11 +0530

    front end completed ,still scroll problem

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBMhN4x2lQ0T6L-rp374m5GXIFwUaRHTvk",
+  authDomain: "movie-streamer-8467f.firebaseapp.com",
+  projectId: "movie-streamer-8467f",
+  storageBucket: "movie-streamer-8467f.appspot.com",
+  messagingSenderId: "442597318866",
+  appId: "1:442597318866:web:e6513ae099df96c120cd0b",
+  measurementId: "G-MMDZNSZBR2"
+};
